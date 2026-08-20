74f76895ab62d03ea8bddd99d70a83f8a0b361e4
kavyanjali-2004 <ponugotinasararao@gmail.com>
2026-08-20 13:08:51 +0530

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # Test vulnerability for Bandit
 # Real Bandit vulnerability
 # Gitleaks test secret
-API_KEY = "ghp_000000000000000000000000000000000000"
+# Gitleaks trigger
+SLACK_WEBHOOK = "https://hooks.slack.com/services/T00/B00/X00"
